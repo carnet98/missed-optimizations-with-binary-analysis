60cb219ad72dd17436b84128bc84106eb1e78783
Colin Arnet <colin98@bluewin.ch>
2024-01-11 10:29:53 +0100

    removed additonal programs

--- data2/program_25/reduced_program_25.c
+++
@@ -1,8 +0,0 @@
-#include "csmith.h"
-uint32_t a, b;
-static int32_t c;
-uint32_t d() {
-  c = 0;
-  return b;
-}
-int main() { a = c; }
--- data2/program_29/reduced_program_29.c
+++
@@ -1,9 +0,0 @@
-#include "csmith.h"
-static uint8_t a = 6;
-uint32_t b() {
-  if (a)
-    for (a = 5; a <= 41; a = safe_add_func_uint16_t_u_u(a, 9))
-      ;
-  return 0;
-}
-int main() { b(); }
