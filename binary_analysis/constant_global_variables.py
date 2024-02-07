56ff11a8046b9f006815170377115fb779cdde94
Colin Arnet <colin98@bluewin.ch>
2024-02-07 13:28:09 +0100

    ready for data collection

--- binary_analysis/constant_global_variables.py
+++ binary_analysis/constant_global_variables.py
@@ -156,19 +156,22 @@ def main():
             compiled, project, globals = binary_analysis_utils.compile_globals_project(program, setting)
             try:
                 cfg = binary_analysis_utils.get_cfg(project)
-                node_num[setting] = len(cfg.graph.nodes)
-                edge_num[setting] = len(cfg.graph.edges)
+                node_num[setting_str] = len(cfg.graph.nodes)
+                edge_num[setting_str] = len(cfg.graph.edges)
                 data = binary_analysis_utils.variable_analysis(project, cfg, globals)
                 data.to_csv(dir_name + "/" + setting_str + ".csv", sep=",", index=False)
             except:
                 print("EXCEPTION")
                 continue
-        node_ratio = node_num[clang_3] / node_num[gcc_3]
-        edge_ratio = edge_num[clang_3] / edge_num[gcc_3]
-        ratio_str = "node ratio," + str(node_ratio) + "\nedge ratio," + str(edge_ratio)
-        f = open(dir_name + "/cfg_data.txt", "w")
-        f.write(ratio_str)
-        f.close()
+        try:
+            node_ratio = node_num[clang_3] / node_num[gcc_3]
+            edge_ratio = edge_num[clang_3] / edge_num[gcc_3]
+            ratio_str = "node ratio," + str(node_ratio) + "\nedge ratio," + str(edge_ratio)
+            f = open(dir_name + "/cfg_data.txt", "w")
+            f.write(ratio_str)
+            f.close()
+        except:
+            print("ERROR in ratio calculation")
         counter += 1
         end_time = time.time()
         runtime = end_time - start_time
